96eb464e2ebcd43493424b26f929fe53eeaedabb
huangming <1635115008@qq.com>
2019-10-16 03:55:00 +0800

    fix some bugs

--- src/cn/aftertomorrow/main/ClassPathXmlApplicationContext.java
+++ src/cn/aftertomorrow/main/ClassPathXmlApplicationContext.java
@@ -1,6 +1,8 @@
 package cn.aftertomorrow.main;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,12 +18,6 @@ public class ClassPathXmlApplicationContext implements BeanFactory {
 	private Map<String, Object> beans = new HashMap<>();
 	// 所有bean的配置信息
 	private Map<String, Bean> beanConfigs = new HashMap<>();
-
-	public ClassPathXmlApplicationContext() {
-		super();
-		// TODO Auto-generated constructor stub
-
-	}
 
 	public ClassPathXmlApplicationContext(String xmlPath) {
 		super();
@@ -46,7 +42,7 @@ public class ClassPathXmlApplicationContext implements BeanFactory {
 		return bean;
 	}
 
-	public Object createBean(String name) {
+	private Object createBean(String name) {
 		Object bean = null;
 		Field field = null;
 		try {
@@ -57,41 +53,50 @@ public class ClassPathXmlApplicationContext implements BeanFactory {
 			List<Property> properties = beanConfig.getProperties();
 			for (Property property : properties) {
 				field = clazz.getDeclaredField(property.getName());
+				Class<?> fieldType = field.getType();
 				System.out.println(property.getName());
-				field.setAccessible(true);
+				Method m = clazz.getDeclaredMethod(getSetterName(property.getName()), fieldType);
 				if (property.getRef() != null) {
-					field.set(bean, getBean(property.getRef()));
+					m.invoke(bean, getBean(property.getRef()));
 				}
 				if (property.getValue() != null) {
-					field.set(bean, convertType(field.getType(), property.getValue()));
+					m.invoke(bean, convertType(field.getType(), property.getValue()));
 				}
 			}
 			beans.put(name, bean);
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InstantiationException e) {
+		} catch (NoSuchMethodException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SecurityException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return bean;
 	}
 
-	public Object convertType(Class<?> className, String value) {
+	private Object convertType(Class<?> className, String value) {
 		Object obj = null;
-		if (className.equals(int.class) || className.equals(Integer.class)) {
+		if (className.equals(String.class)) {
+			obj = value;
+		} else if (className.equals(int.class) || className.equals(Integer.class)) {
 			obj = Integer.valueOf(value);
 		} else if (className.equals(byte.class) || className.equals(Byte.class)) {
 			obj = Byte.valueOf(value);
@@ -107,59 +112,63 @@ public class ClassPathXmlApplicationContext implements BeanFactory {
 			obj = Boolean.valueOf(value);
 		} else if (className.equals(double.class) || className.equals(Double.class)) {
 			obj = Double.valueOf(value);
-		} else if (className.equals(String.class)) {
-			obj = value;
 		}
-		/*
-		 * else { try {
-		 * className.getConstructor(String.class).newInstance(value); } catch
-		 * (InstantiationException e) { // TODO Auto-generated catch block
-		 * e.printStackTrace(); } catch (IllegalAccessException e) { // TODO
-		 * Auto-generated catch block e.printStackTrace(); } catch
-		 * (IllegalArgumentException e) { // TODO Auto-generated catch block
-		 * e.printStackTrace(); } catch (InvocationTargetException e) { // TODO
-		 * Auto-generated catch block e.printStackTrace(); } catch
-		 * (NoSuchMethodException e) { // TODO Auto-generated catch block
-		 * e.printStackTrace(); } catch (SecurityException e) { // TODO
-		 * Auto-generated catch block e.printStackTrace(); } }
-		 */
 		System.out.println(obj);
 		return obj;
 	}
 
-	public Map<String, Object> getBeans() {
+	private Map<String, Object> getBeans() {
 		return beans;
 	}
 
-	public void setBeans(Map<String, Object> beans) {
+	private void setBeans(Map<String, Object> beans) {
 		this.beans = beans;
 	}
 
-	public void setBeanConfigs(Map<String, Bean> beanConfigs) {
+	private void setBeanConfigs(Map<String, Bean> beanConfigs) {
 		this.beanConfigs = beanConfigs;
 	}
 
-	public Map<String, Bean> getBeanConfigs() {
+	private Map<String, Bean> getBeanConfigs() {
 		return beanConfigs;
+	}
+
+	private String getSetterName(String name) {
+		return "set" + new StringBuffer(name).replace(0, 1, name.substring(0, 1).toUpperCase());
 	}
 
 	public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, SecurityException,
 			IllegalArgumentException, IllegalAccessException, InstantiationException {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				"cn/aftertomorrow/config/applicationContext.xml");
-		/*
-		 * Bean b bean = new Bean(); bean.setClazz("cn.aftertomorrow.po.Phone");
-		 * bean.setId("phone"); Property property1 = new Property();
-		 * property1.setName("brand"); property1.setValue("meizu");
-		 * bean.getProperties().add(property1); Property property2 = new
-		 * Property(); property2.setName("model");
-		 * property2.setValue("pro6plus"); bean.getProperties().add(property2);
-		 * Property property3 = new Property(); property3.setName("price");
-		 * property3.setValue("2999"); bean.getProperties().add(property3);
-		 * System.out.println(bean); context.getBeanConfigs().put("phone",
-		 * bean);
-		 */
-		System.out.println(context.getBean("person"));
+		System.out.println(context.getBean("phone"));
+		System.out.println(context.getBean("animal"));
+		// try {
+		// Method m = String.class.getDeclaredMethod("equals", String.class);
+		// System.out.println(m.invoke(new String("hello"), "hello"));
+		// } catch (NoSuchMethodException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// } catch (InvocationTargetException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		// System.out.println(context.getBean("person"));
+		// int i1 = 0b10000000000000000000000000000000;
+		// int i2 = 0b10000000000000000000000000000001;
+		// 10000000000000000000000000000001
+		// 11111111111111111111111111111111
+		// System.out.println(i1);
+		// System.out.println(i2);
+		// System.out.println(Integer.toBinaryString(-2147483648));
 	}
-
+	// 原00000001
+	// 反00000001
+	// 补00000001
+	// 原10000011
+	// 反11111100
+	// 补11111101
+	// 补码相加11111110
+	// 补码的反码10000001
+	// 补码的补码10000010
 }
---
+++ src/cn/aftertomorrow/po/Animal.java
@@ -0,0 +1,28 @@
+package cn.aftertomorrow.po;
+
+public class Animal {
+	private String name;
+	private String kind;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getKind() {
+		return kind;
+	}
+
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
+
+	@Override
+	public String toString() {
+		return "Animal [name=" + name + ", kind=" + kind + "]";
+	}
+
+}
